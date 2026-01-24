57139b96e574ae2c32f8ed393d717cda905f3ef3
The-Simomatic <thesimomatic@gmail.com>
2026-01-24 10:22:16 +0100

    retirer les secrets

--- cv_sportif.py
+++
@@ -1,7 +0,0 @@
-import mesop as me
-import styles as st
-
-def cv_screen(s):
-    with me.box(style=me.Style(margin=me.Margin(top=20))):
-        me.text("PAGE CV SPORTIF", style=st.LOGIN_TITLE_STYLE)
-        me.text("Le CV Sportif est en cours de construction...", style=me.Style(font_style="italic"))
